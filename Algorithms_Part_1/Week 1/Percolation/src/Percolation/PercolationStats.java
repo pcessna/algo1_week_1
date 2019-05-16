3bf54936cba74a204ed18c91673353d01e05cbb7
Cody <Cody@Katelyn-PC>
2019-05-15 22:48:02 -0500

    Completes assignment. May need cleaning up.

--- Algorithms_Part_1/Week 1/Percolation/src/Percolation/PercolationStats.java
+++ Algorithms_Part_1/Week 1/Percolation/src/Percolation/PercolationStats.java
@@ -1,62 +1,67 @@
 package Percolation;
+
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
-			
-			public int trialsUntilPercolate = 0;
-			double[] thresholds;
-			
-		   public PercolationStats(int n, int trials)// perform trials independent experiments on an n-by-n grid
-		   {
-			   
-			   Percolation testCase = new Percolation(n);  
-			   thresholds = new double[trials];
-			   int runCount=0;
-			   int r1;
-			   int r2;
-			   for(int i = 0; i < trials; i++) {
-				   runCount = 0;
-				   testCase = new Percolation(n);
-				   while(!testCase.percolates()) {
-					   r1 = StdRandom.uniform(1,n+1);
-					   r2 = StdRandom.uniform(1,n+1);
-					   if(!testCase.isOpen(r1,r2)) {
-						   testCase.open(r1,r2);
-						   runCount++;
-					   }
-					  }
-				   thresholds[i] = (double)runCount / (double)(n*n);
-					}
-			   }
-		
-				 
-		   public double mean()                          // sample mean of percolation threshold
-		   {
-			   return 1.3;
-		   }
-		   public double stddev()                        // sample standard deviation of percolation threshold
-		   {
-			   return 1.3;
-		   }
-		   
-		   public double confidenceLo()                  // low  endpoint of 95% confidence interval
-		   {
-			   return 1.3;
-		   }
-		   public double confidenceHi()                  // high endpoint of 95% confidence interval
-		   {
-			   return 1.3;
-		   }
-		   
-		   public static void main(String[] args)        // test client (described below)
-		   {
-			   int gridSize = 4;
-			   int trials = 10;
-			   PercolationStats test1 = new PercolationStats(gridSize,trials);
-			   for(int i = 0; i < test1.thresholds.length; i++) {
-				   System.out.print("\n" + test1.thresholds[i]);
-				   
-			   }
-		   }
+
+	private int numTrials;
+	double[] thresholds;
+
+	public PercolationStats(int n, int trials)// perform trials independent experiments on an n-by-n grid
+	{
+
+		Percolation testCase = new Percolation(n);
+		thresholds = new double[trials];
+		numTrials = trials;
+		int runCount = 0;
+		int r1;
+		int r2;
+		for (int i = 0; i < numTrials; i++) {
+			runCount = 0;
+			testCase = new Percolation(n);
+			while (!testCase.percolates()) {
+				r1 = StdRandom.uniform(1, n + 1);
+				r2 = StdRandom.uniform(1, n + 1);
+				if (!testCase.isOpen(r1, r2)) {
+					testCase.open(r1, r2);
+					runCount++;
+				}
+			}
+			thresholds[i] = (double) runCount / (double) (n * n);
+		}
+	}
+
+	public double mean() // sample mean of percolation threshold
+	{
+		return StdStats.mean(thresholds);
+	}
+
+	public double stddev() // sample standard deviation of percolation threshold
+	{
+		return StdStats.stddev(thresholds);
+	}
+
+	public double confidenceLo() // low endpoint of 95% confidence interval
+	{
+		return mean() - ((1.96 * stddev()) / Math.sqrt(numTrials));
+	}
+
+	public double confidenceHi() // high endpoint of 95% confidence interval
+	{
+		return mean() + ((1.96 * stddev()) / Math.sqrt(numTrials));
+	}
+
+	public static void main(String[] args) // test client (described below)
+	{
+		int N = Integer.parseInt(args[0]);
+		int T = Integer.parseInt(args[1]);
+		PercolationStats ps = new PercolationStats(N, T);
+		String confidence = ps.confidenceLo() + ", " + ps.confidenceHi();
+		StdOut.println("mean                    = " + ps.mean());
+		StdOut.println("stddev                  = " + ps.stddev());
+		StdOut.println("95% confidence interval = " + confidence);
+	}
 
 }
